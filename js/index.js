2d9db35a0ea9a5e57cabdcc34efb9fba4c5fe881
BNanyingi <nanyingibarbara@gmail.com>
2021-05-26 22:52:58 +0300

    update

--- js/index.js
+++ js/index.js
@@ -1,3 +1,19 @@
+
+/* sidenav*/
+
+document.getElementById('moreBtn').addEventListener('click',
+  function(){
+    const tats = document.querySelector('.section');
+    tats.classList.toggle('move', behavior = 'smooth');
+  });
+document.getElementById('close').addEventListener('click',
+  function(){
+    const stat = document.querySelector('.section');
+    stat.classList.remove('move');
+  });
+
+  
+
 let i = 0;
 
 let box1 = document.getElementById("image1");
@@ -127,3 +143,203 @@ function left() {
         i = 0;
     }
 }
+
+// used to avoid using 255, thus generating white-ish backgrounds that make text unreadable
+const colorMax = 192;
+
+// gets the breakpoint at which the scroll-to-top button should appear
+const scrollBreakpoint = window.innerHeight * 0.9;
+
+document.addEventListener('DOMContentLoaded', () => {
+  randomizeBackgrounds();
+  setupScrollListener();
+  setupScrollEvent();  
+});
+
+// scrolls window to top
+function setupScrollEvent() {
+  const scrollButton = document.querySelector('.scroll-top');
+  
+  scrollButton.addEventListener('click', (e) => {
+    // not the best solution until Safari/Edge support scroll behavior
+    // window.scrollTo({ top: 0, behavior: 'smooth' });
+    
+    // Thanks to Geroge Daniel https://stackoverflow.com/questions/51229742/javascript-window-scroll-behavior-smooth-not-working-in-safari
+    smoothVerticalScrolling(scrollButton.parentElement, 250, "top");
+  });
+}
+
+// prepares the window for a scroll event to show the scroll button
+function setupScrollListener() {  
+   window.addEventListener('scroll', (e) => {
+     const scrollButton = document.querySelector('.scroll-top');
+     
+     // const scrollOffset = document.scrollingElement.scrollTop;
+     const scrollOffset = window.scrollY;
+    
+     if (scrollOffset >= scrollBreakpoint) {
+       scrollButton.classList.add('visible');
+     } else if (scrollOffset <= 0) {
+       scrollButton.classList.remove('visible');
+     }    
+  });
+}
+
+function randomizeBackgrounds() {
+  // get all the content containers
+  const contentContainers = document.querySelectorAll('.content-container');
+  
+  [].forEach.call(contentContainers, container => {
+    // assign random background
+    container.style.background = `rgb(${randVal(colorMax)},${randVal(colorMax)},${randVal(colorMax)})`;
+  });
+}
+
+// random between 0 to max
+function randVal(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+// uses a timeout to scroll to top
+function smoothVerticalScrolling(e, time, where) {
+  // gets the element's top position relative to the viewport
+  const eTop = e.getBoundingClientRect().top;
+  
+  // divides the top offset into 100 steps to be ellapsed
+  const eAmt = eTop / 100;
+  
+  // starting time
+  let curTime = 0;
+  
+  // not to exceed the desired duration
+  while (curTime <= time) {
+    // call a function to execute at one hundreth of the desired scroll time
+    window.setTimeout(SVS_B, curTime, eAmt, where);
+    // increase by one hundreth of the desired time to execute exactly 100 times
+    curTime += time / 100;
+  }
+}
+
+function SVS_B(eAmt, where) {
+  // scroll by half the hundredth of the top offset if destination is not top (since to center only involves scrolling either in the top or bottom half of the window)
+  if(where == "center" || where == "") {
+    window.scrollBy(0, eAmt / 2); 
+  }
+  // otherwise scroll the full amount
+  if (where == "top") {
+    window.scrollBy(0, eAmt);
+  }    
+}
+
+var ml4 = {};
+ml4.opacityIn = [0,1];
+ml4.scaleIn = [0.2, 1];
+ml4.scaleOut = 3;
+ml4.durationIn = 800;
+ml4.durationOut = 600;
+ml4.delay = 500;
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml4 .letters-1',
+    opacity: ml4.opacityIn,
+    scale: ml4.scaleIn,
+    duration: ml4.durationIn
+  }).add({
+    targets: '.ml4 .letters-1',
+    opacity: 0,
+    scale: ml4.scaleOut,
+    duration: ml4.durationOut,
+    easing: "easeInExpo",
+    delay: ml4.delay
+  }).add({
+    targets: '.ml4 .letters-2',
+    opacity: ml4.opacityIn,
+    scale: ml4.scaleIn,
+    duration: ml4.durationIn
+  }).add({
+    targets: '.ml4 .letters-2',
+    opacity: 0,
+    scale: ml4.scaleOut,
+    duration: ml4.durationOut,
+    easing: "easeInExpo",
+    delay: ml4.delay
+  }).add({
+    targets: '.ml4 .letters-3',
+    opacity: ml4.opacityIn,
+    scale: ml4.scaleIn,
+    duration: ml4.durationIn
+  }).add({
+    targets: '.ml4 .letters-3',
+    opacity: 0,
+    scale: ml4.scaleOut,
+    duration: ml4.durationOut,
+    easing: "easeInExpo",
+    delay: ml4.delay
+  }).add({
+    targets: '.ml4 .letters-4',
+    opacity: ml4.opacityIn,
+    scale: ml4.scaleIn,
+    duration: ml4.durationIn
+  }).add({
+    targets: '.ml4 .letters-4',
+    opacity: 0,
+    scale: ml4.scaleOut,
+    duration: ml4.durationOut,
+    easing: "easeInExpo",
+    delay: ml4.delay
+  }).add({
+    targets: '.ml4 .letters-5',
+    opacity: ml4.opacityIn,
+    scale: ml4.scaleIn,
+    duration: ml4.durationIn
+  }).add({
+    targets: '.ml4 .letters-5',
+    opacity: 0,
+    scale: ml4.scaleOut,
+    duration: ml4.durationOut,
+    easing: "easeInExpo",
+    delay: ml4.delay
+  }).add({
+    targets: '.ml4 .letters-6',
+    opacity: ml4.opacityIn,
+    scale: ml4.scaleIn,
+    duration: ml4.durationIn
+  }).add({
+    targets: '.ml4 .letters-6',
+    opacity: 0,
+    scale: ml4.scaleOut,
+    duration: ml4.durationOut,
+    easing: "easeInExpo",
+    delay: ml4.delay
+  }).add({
+    targets: '.ml4',
+    opacity: 0,
+    duration: 500,
+    delay: 500
+  });
+
+/**Client area */
+
+$(document).ready(function(){
+  $('.customer-logos').slick({
+      slidesToShow: 6,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 1500,
+      arrows: false,
+      dots: false,
+      pauseOnHover: false,
+      responsive: [{
+          breakpoint: 768,
+          settings: {
+              slidesToShow: 4
+          }
+      }, {
+          breakpoint: 520,
+          settings: {
+              slidesToShow: 3
+          }
+      }]
+  });
+});
